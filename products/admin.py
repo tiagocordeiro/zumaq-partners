f634d8e0c20b00fd620ee8e57ed7b9153b9ce5f6
Tiago Cordeiro <tiago@mulhergorila.com>
2019-01-10 02:02:16 -0200

    Configura Produtos no Admin do Django

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from .models import Produto
+
 
 # Register your models here.
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'descricao')
+
+
+admin.site.register(Produto, ProdutoAdmin)
