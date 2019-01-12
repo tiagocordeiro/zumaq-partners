577a7bdec065515dcb5e7b6607d8fb3a365e70ba
Tiago Cordeiro <tiago@mulhergorila.com>
2019-01-12 15:09:31 -0200

    Melhora cobertura de testes

--- products/tests.py
+++ products/tests.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 
-from django.contrib.auth.models import User, Group, AnonymousUser
+from django.contrib.auth.models import User, Group
 from django.test import TestCase, RequestFactory, Client
 from django.urls import reverse
 
@@ -13,10 +13,18 @@ class ProductsTestCase(TestCase):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
         self.client = Client()
-        self.user_gerente = User.objects.create_user(username='jacob', email='jacob@…', password='top_secret')
-        self.group = Group.objects.create(name='Gerente')
-        self.group.user_set.add(self.user_gerente)
 
+        # User Gerente
+        self.user_gerente = User.objects.create_user(username='jacob', email='jacob@…', password='top_secret')
+        self.group_gerente = Group.objects.create(name='Gerente')
+        self.group_gerente.user_set.add(self.user_gerente)
+
+        # User Parceiro
+        self.user_parceiro = User.objects.create_user(username='joe', email='joe@…', password='top_secret')
+        self.group_parceiro = Group.objects.create(name='Parceiro')
+        self.group_parceiro.user_set.add(self.user_parceiro)
+
+        # Produto
         self.product = Produto.objects.create(codigo='TYL-1080',
                                               descricao='Tubo de Laser Yong Li - 80w - R3',
                                               pago_na_china=880,
@@ -51,19 +59,12 @@ class ProductsTestCase(TestCase):
         self.assertEqual(produto.unitario_em_dolar(), Decimal('308.41'))
 
     def test_product_add_anonimo(self):
-        request = self.factory.get(reverse('product_add'))
-        request.user = AnonymousUser()
+        self.client.logout()
+        response = self.client.get(reverse('product_add'))
 
-        response = product_add(request)
         self.assertEqual(response.status_code, 302)
-        # request = self.factory.get(reverse('product_add'))
-        # self.client.force_login(self.user_gerente)
-        # request.user = AnonymousUser()
-
-        # response = product_add(request)
-        # response = self.client.get(reverse('product_add'))
-        # self.assertEqual(response.status_code, 302)
-        # self.assertRedirects(response, '/accounts/login/?next=/products/product/add/')
+        self.assertRedirects(response, '/accounts/login/?next=/products/product/add/',
+                             status_code=302, target_status_code=200)
 
     def test_product_add_gerente(self):
         request = self.factory.get(reverse('product_add'))
@@ -102,7 +103,36 @@ class ProductsTestCase(TestCase):
     def test_product_create_exist_status_code_gerente(self):
         self.client.force_login(self.user_gerente)
         response = self.client.post(reverse('product_create', kwargs={'codigo': self.product.codigo}))
-        print(response)
 
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/products/product/update/TYL-1080/', status_code=302, target_status_code=200)
+
+    def test_product_update_does_not_exist(self):
+        self.client.force_login(self.user_gerente)
+        response = self.client.post(reverse('product_update', kwargs={'codigo': 'AAA'}))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/products/product/create/AAA/', status_code=302, target_status_code=302)
+
+    def test_product_list_view_anonimo(self):
+        self.client.logout()
+        response = self.client.get(reverse('product_list'))
+
+        # response = product_list(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/accounts/login/?next=/products/list/',
+                             status_code=302, target_status_code=200)
+
+    def test_product_list_view_gerente(self):
+        self.client.force_login(self.user_gerente)
+        response = self.client.get(reverse('product_list'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'TYL-1080')
+
+    def test_product_view_gerente(self):
+        self.client.force_login(self.user_gerente)
+        response = self.client.get(reverse('product_view', kwargs={'codigo': self.product.codigo}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'TYL-1080')
