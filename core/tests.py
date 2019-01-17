67065e94a7e918af9b55b4d5371429fc2b9d1b8d
Tiago Cordeiro <tiago@mulhergorila.com>
2019-01-17 15:49:56 -0200

    Melhora cobertura de testes

--- core/tests.py
+++ core/tests.py
@@ -6,6 +6,7 @@ from io import BytesIO
 
 from django.contrib.auth.models import AnonymousUser, User, Group
 from django.test import RequestFactory, TestCase, Client, override_settings
+from django.urls import reverse
 
 from .models import UserProfile
 from .views import dashboard, profile_update
@@ -112,3 +113,80 @@ class ProfileUpdateViewTest(TestCase):
         self.user_parceiro.refresh_from_db()
         self.assertEqual(response.status_code, 302)
         self.assertEqual(self.user_parceiro.first_name, 'Joe')
+
+class ParceirosViewsTests(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.client = Client()
+
+        # User Gerente
+        self.user_gerente = User.objects.create_user(username='jacob', email='jacob@…', password='top_secret')
+        self.group_gerente = Group.objects.create(name='Gerente')
+        self.group_gerente.user_set.add(self.user_gerente)
+
+        # User Parceiro
+        self.user_parceiro = User.objects.create_user(username='joe', email='joe@…', password='top_secret')
+        self.group_parceiro = Group.objects.create(name='Parceiro')
+        self.group_parceiro.user_set.add(self.user_parceiro)
+
+        # User Profile Parceiro
+        image_thumb = '''
+                R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7
+                '''.strip()
+
+        self.image = InMemoryUploadedFile(
+            BytesIO(base64.b64decode(image_thumb)),  # use io.BytesIO
+            field_name='tempfile',
+            name='tempfile.png',
+            content_type='image/png',
+            size=len(image_thumb),
+            charset='utf-8',
+        )
+
+        self.user_profile_parceiro = UserProfile.objects.create(user=self.user_parceiro, avatar=str(self.image))
+
+    def test_view_list_parceiros(self):
+        self.client.force_login(self.user_gerente)
+        request = self.client.get(reverse('parceiro_list'))
+
+        self.assertEqual(request.status_code, 200)
+
+        # Adiciona novo parceiro
+        novo_parceiro = User.objects.create_user(username='neo', email='neo@…', password='top_secret')
+        self.group_parceiro.user_set.add(novo_parceiro)
+
+        request = self.client.get(reverse('parceiro_list'))
+        self.assertEqual(request.status_code, 200)
+        parceiros = User.objects.filter(groups__name__in=['Parceiro'])
+        parceiros.delete()
+
+        request = self.client.get(reverse('parceiro_list'))
+        self.assertEqual(request.status_code, 200)
+
+    def test_view_cadastro_parceiro(self):
+        self.client.force_login(self.user_gerente)
+        request = self.client.get(reverse('parceiro_cadastro'))
+
+        self.assertEqual(request.status_code, 200)
+
+    def test_view_cadastro_parceiro_post(self):
+        new_user = {'username': 'newuser',
+                    'email': 'newuser@foo.bar',
+                    'first_name': 'New',
+                    'last_name': 'User',
+                    'password1': 'top_secret',
+                    'password2': 'top_secret', }
+
+        parceiros = User.objects.filter(groups__name__in=['Parceiro'])
+        self.assertEqual(len(parceiros), 1)
+
+        self.client.force_login(self.user_gerente)
+        response = self.client.post(reverse('parceiro_cadastro'), data=new_user)
+
+        new_user = User.objects.get(username='newuser')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(new_user.first_name, 'New')
+
+        parceiros = User.objects.filter(groups__name__in=['Parceiro'])
+        self.assertEqual(len(parceiros), 2)
