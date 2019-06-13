f2e05456568a4b0c56ecdfe859ea153eec0a91c2
Tiago Cordeiro <tiago@mulhergorila.com>
2019-06-13 16:45:20 -0300

    Melhora performance carregamento de imagens.
    This close #163

--- core/templatetags/core_extras.py
+++ core/templatetags/core_extras.py
@@ -18,3 +18,8 @@ def subtotal(valor_un, quantidade):
 @register.filter(name='normalize_for_table')
 def normalize_for_table(text):
     return normalize('NFKD', text).encode('ASCII', 'ignore').decode('ASCII')
+
+
+@register.filter(name='load_thumb')
+def load_thumb(image_url):
+    return image_url.replace('800x800', '100x100')
