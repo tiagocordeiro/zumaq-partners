543c1b54ad478a18c71a0882aa7a688c21cf24e2
Tiago Cordeiro <tiago@mulhergorila.com>
2020-10-22 02:52:54 -0300

    SetUp new api urls
    Configura novos endpoints para a API.
    - /products/json/public/`TOKEN`
    - /products/json/public/`CODIGO_PRODUTO`/`TOKEN`
    
    e com token enviado no cabeçalho requisição.
    - /api/products/
    - /api/product/`CODIGO_PRODUTO`/
    `'Token': 'TOKEN'`

--- products/urls.py
+++ products/urls.py
@@ -3,14 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.product_list, name='product_list'),
-    path('list/atacado/', views.product_atacado_list, name='product_list_atacado'),
-    path('product/add/', views.product_add, name='product_add'),
-    path('product/<str:codigo>/', views.product_view, name='product_view'),
-    path('product/create/<str:codigo>/', views.product_create, name='product_create'),
-    path('product/update/<str:codigo>/', views.product_update, name='product_update'),
-    path('json/', views.product_list_json, name='product_list_json'),
-    path('json/<str:codigo>/', views.product_detail_json, name='product_detail_json'),
-    path('api/<str:secret_key>/', views.api_product_list, name='api_product_list'),
-    path('api/<str:codigo>/<str:secret_key>/', views.api_product_detail, name='api_product_detail'),
+    path('products/list/', views.product_list, name='product_list'),
+    path('products/list/atacado/', views.product_atacado_list, name='product_list_atacado'),
+    path('products/product/add/', views.product_add, name='product_add'),
+    path('products/product/<str:codigo>/', views.product_view, name='product_view'),
+    path('products/product/create/<str:codigo>/', views.product_create, name='product_create'),
+    path('products/product/update/<str:codigo>/', views.product_update, name='product_update'),
+    path('products/json/', views.product_list_json, name='product_list_json'),
+    path('products/json/<str:codigo>/', views.product_detail_json, name='product_detail_json'),
+    path('products/json/public/<str:secret_key>/', views.api_product_list, name='api_product_list'),
+    path('products/json/public/<str:codigo>/<str:secret_key>/', views.api_product_detail, name='api_product_detail'),
+    path('api/products/', views.api_product_list_header_token, name='api_product_list_header_token'),
+    path('api/product/<str:codigo>/', views.api_product_detail_header_token, name='api_product_detail_header_token'),
 ]
